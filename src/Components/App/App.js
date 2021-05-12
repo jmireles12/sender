0fb516dab3c875c39af4fbc7371f040cec212184
Juan Mireles <jmireles12@gmail.com>
2021-05-12 12:30:28 -0500

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <Form>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
+
+export default App;
